22b8f7c19a8adaf9ab53486bbbdbb855fe9bc446
bauer <bauemc20@htl-kaindorf.at>
2023-05-12 09:09:50 +0200

    12_05_2023 addSHopInterfaceAndMockData

---
+++ campus/src/common/mock_data_shop.ts
@@ -0,0 +1,18 @@
+import {IShop} from "./models/IShop";
+
+export const mock_data_shops: IShop[] = [
+    {
+        id: 1,
+        name: "Billa"
+    },
+    {
+        id: 2,
+        name: "Spar"
+    },
+    {
+        id: 3,
+        name: "Hofer"
+    },
+]
+
+
