c846302f1d219c1bb95d4fbfb0ed0d6a13ba128c
bauer <bauemc20@htl-kaindorf.at>
2023-05-30 09:08:55 +0200

    30_05_2023 stuff

--- campus/src/components/Homepage/Delivery.tsx
+++ campus/src/components/Homepage/Delivery.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import {IDelivery} from "../../common/models/IDelivery";
+import { Route, useNavigate } from "react-router-dom";
 
 interface DeliveryProps {
     delivery: IDelivery
 }
 
 const Delivery: React.FC<DeliveryProps> = ({delivery}) => {
+
+    const navigate = useNavigate();
+    const handleOnCLick = () => {
+        navigate('/login');
+    }
     return (
        <tr>
            <td>{delivery.userID}</td>
-           <td>{delivery.shop}</td>
+           <td onClick={handleOnCLick}>{delivery.shop}</td>
        </tr>
     );
 };
